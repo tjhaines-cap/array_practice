c1f1e3b69d5fefa6834d25051cbd04b9d8cf76ff
tjhaines-cap <tjhaines@eckerd.edu>
2022-03-08 10:38:23 -0700

    Alter each array by one element

--- array.rb
+++ array.rb
@@ -4,4 +4,17 @@
  ages = [20, 24, 34, 28]
  floats = [3.5, 2.2, 8.4, 9.36]
  booleans = [true, false, false, true]
+
+ #Pop will remove the last element of the array
+ disney_movies.pop
+
+ #push will add an element to the end of the arrays
+ ages.push(18)
+
+ #shift will remove the first element of the arrays
+ floats.shift
+
+ #unshift will add an element to the beginning of the array
+ floats.unshift(flase)
+
  
