a9abc61fbd02b49fcd9336eb50ae732c3f9c2d6f
tjhaines-cap <tjhaines@eckerd.edu>
2022-03-08 10:32:30 -0700

    Create arrays of each data type

---
+++ array.rb
@@ -0,0 +1,7 @@
+# Ruby arrays practice
+
+ disney_movies = ["Pinochio", "Bambi", "Snow White", "Peter Pan"]
+ ages = [20, 24, 34, 28]
+ floats = [3.5, 2.2, 8.4, 9.36]
+ booleans = [true, false, false, true]
+ 
